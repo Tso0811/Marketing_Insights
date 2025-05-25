f6be619f7400e337696341290748115935bd14af
Tso0811 <savagetso0811@gmail.com>
2025-05-25 18:45:15 +0800

    完成campaign CRUD

---
+++ dashboard/urls.py
@@ -0,0 +1,12 @@
+#campings app urls.py
+from django.contrib import admin
+from django.urls import path
+from . import views
+app_name = 'dashboard'
+
+urlpatterns = [
+    path('' , views.show_campaigns , name='show_campaigns'),
+    path('campaign_edit/<int:id>' , views.edit_campaigns , name='campaign_edit'),
+    path('campaign_create/' , views.campaign_create , name='campaign_create'),
+    path('campaign_delete/<int:id>' , views.campaign_delete , name='campaign_delete'),
+]
---
+++ dashboard/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from campaigns.models import Campaigns
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404 ,redirect
+
+# Create your views here.
+
+def show_campaigns(request):    #直接在dashboard中顯示 不需要點擊進入細節
+    campaign = Campaigns.objects.all()
+    return render(request , 'dashboard.html' , {'campaigns':campaign})
+
+@login_required
+def edit_campaigns(request , id):
+    campaign = get_object_or_404(Campaigns , pk=id)
+    if request.method == 'POST':
+        new_name = request.POST['name']
+        new_start_date = request.POST['start_date']
+        new_budget = request.POST['budget']
+
+        campaign.name = new_name
+        campaign.start_date = new_start_date
+        campaign.budget = new_budget
+
+        campaign.save()
+
+        return redirect('dashboard:show_campaigns')
+
+    return render(request, 'campaign_form.html', {'campaign': campaign})
+
+def campaign_create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        start_date = request.POST.get('start_date')
+        budget = request.POST.get('budget')
+
+        new_campaign = Campaigns(name=name , start_date=start_date , budget=budget)
+        new_campaign.save()
+
+        return redirect('dashboard:show_campaigns')
+
+    return render(request , 'campaign_form.html')
+
+def campaign_delete (request , id):
+    campaign = get_object_or_404(Campaigns , pk=id)
+    if request.method == 'POST':
+        campaign.delete()
+        return redirect('dashboard:show_campaigns')
+    return render(request , 'campaign_delete.html' , {'campaign':campaign})
