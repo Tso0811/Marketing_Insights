5a4acb1276a3f964c295337cf2996602f454ba9d
Tso0811 <savagetso0811@gmail.com>
2025-05-31 22:41:31 +0800

    移除資料庫檔案的 Git 追蹤 並加入顯示活動上傳者功能

--- campaigns/models.py
+++ campaigns/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Campaigns(models.Model):
     budget = models.IntegerField()
     click = models.IntegerField(default=0)
 
+    poster = models.ForeignKey(User , on_delete=models.CASCADE)
     def __str__(self):
         return (self.name)
     
